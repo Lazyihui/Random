6fece3a9e8eda9c5da708730d61155a442a273e1
包子 <chengbaozi6@qq.com>
2024-04-23 20:43:11 +0800

    es

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class XYL {
+    public static void Main() {
+        XYL.rew();
+    }
+    public static void rew() {
+        Console.WriteLine("hesss");
+        Console.ReadKey();
+    }
+}
